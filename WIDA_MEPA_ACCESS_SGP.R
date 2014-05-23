9e626eaadfe51a5dd0d2fd68d0b3ff77838c16ca
WirelessK19 <kathleenflanagan19@gmail.com>
2014-05-23 14:51:32 -0400

    Updating SGP gitignore and SGP script

--- WIDA_MEPA_ACCESS_SGP.R
+++ WIDA_MEPA_ACCESS_SGP.R
@@ -1,54 +1,31 @@
 ########################################################################################
 ###
-### Data preparation code for 2014 Access
+### SGP Analyses for 2014 WIDA/MEPA 
 ###
 ########################################################################################
 
 ### Load Packages
 
 require(SGP)
-require(foreign)
-require(data.table)
+
 
 
 ### Load Data
 
-WIDA_MEPA_ACCESS_Data_LONG <- read.spss("Data/Base_Files/MEPA_ACCESS Long with Access Scaled scores_5.5.2014.sav", to.data.frame=TRUE)
+load("Data/WIDA_MEPA_ACCESS_Data_LONG.Rdata")
 
 
-### clean up data
 
-names(WIDA_MEPA_ACCESS_Data_LONG) <- toupper(names(WIDA_MEPA_ACCESS_Data_LONG))
+### abcSGP
 
-names(WIDA_MEPA_ACCESS_Data_LONG) <- c("YEAR", "ID", "LAST_NAME", "FIRST_NAME", "MIDDLE_NAME", "DOB", "GRADE", "GRADESPAN", "SS_LEVEL", "SCALE_SCORE")
-
-WIDA_MEPA_ACCESS_Data_LONG <- subset(WIDA_MEPA_ACCESS_Data_LONG, select=c("YEAR", "ID", "LAST_NAME", "FIRST_NAME", "GRADE", "SCALE_SCORE"))
-WIDA_MEPA_ACCESS_Data_LONG$YEAR <- as.character(WIDA_MEPA_ACCESS_Data_LONG$YEAR)
-WIDA_MEPA_ACCESS_Data_LONG$ID <- as.character(WIDA_MEPA_ACCESS_Data_LONG$ID)
-WIDA_MEPA_ACCESS_Data_LONG$GRADE <- as.character(WIDA_MEPA_ACCESS_Data_LONG$GRADE)
-WIDA_MEPA_ACCESS_Data_LONG <- subset(WIDA_MEPA_ACCESS_Data_LONG, GRADE!="  ")
-
-WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME[WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME=="                  "] <- NA
-WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME[WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME=="              "] <- NA
-WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME <- droplevels(WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME)
-WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME <- droplevels(WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME)
-levels(WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME) <- sapply(levels(WIDA_MEPA_ACCESS_Data_LONG$LAST_NAME), capwords)
-levels(WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME) <- sapply(levels(WIDA_MEPA_ACCESS_Data_LONG$FIRST_NAME), capwords)
-
-WIDA_MEPA_ACCESS_Data_LONG$GRADE <- as.factor(WIDA_MEPA_ACCESS_Data_LONG$GRADE)
-levels(WIDA_MEPA_ACCESS_Data_LONG$GRADE) <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
-WIDA_MEPA_ACCESS_Data_LONG$GRADE <- as.character(WIDA_MEPA_ACCESS_Data_LONG$GRADE)
+WIDA_MEPA_ACCESS_SGP <- abcSGP(
+			WIDA_MEPA_ACCESS_Data_LONG,
+			steps=c("prepareSGP", "analyzeSGP", "combineSGP"), 
+			years="2014")
 
 
-WIDA_MEPA_ACCESS_Data_LONG$VALID_CASE <- "VALID_CASE"
-WIDA_MEPA_ACCESS_Data_LONG$CONTENT_AREA <- "LANGUAGE"
-
-### Reorder 
-
-WIDA_MEPA_ACCESS_Data_LONG <- as.data.table(WIDA_MEPA_ACCESS_Data_LONG)
-setcolorder(WIDA_MEPA_ACCESS_Data_LONG, c(7,8,1,2,3,4,5,6))
 
 
 ### Save data
 
-save(WIDA_MEPA_ACCESS_Data_LONG, file="Data/WIDA_MEPA_ACCESS_Data_LONG.Rdata")
+#save(WIDA_MEPA_ACCESS_SGP, file="Data/WIDA_MEPA_ACCESS_SGP.Rdata")
